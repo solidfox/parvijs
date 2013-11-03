b32a8c250613c825c14cdbd0dada3b5d38091433
solidfox <daniel.schlaug@gmail.com>
2013-11-03 15:02:30 +0800

    Completed the serializer

--- tree_deserializer.js
+++ tree_deserializer.js
@@ -1,55 +1,60 @@
-
-
 function deserialize_tree(serialized_tree) {
 	var bracket_stack = [];
 	var node_stack = [];
 
-	var ws_re = /\s/;
-	var bisegment_re = /^/g;
-	var label_re = /^(\w*)\s*("([^"]|\\")*"\s*\/\s*"([^"]|\\")*")?/g;
+	var current_node;
 
-	for (var i = 0; i < serialized_tree.length; i++) {
-		var token = serialized_tree[i];
-		if (token.test(ws_re)) {continue;}
+	var node_re = /(?:^\s*(<|\[)\s*(\w*)(?:\s+"((?:[^"]|\\")*)"\/"((?:[^"]|\\")*)")?)|(?:^\s*(>|\])\s*)/g;
 
-		switch (token) {
-			case '<':
-			case '[':
-			bracket_stack.push(token);
-			break;
-			case '>':
-			if (bracket_stack.pop() != '<') {
-				throw "Syntax error";
-			}
-			// TODO pop the node_stack into the children of the tail
-			break;
-			case ']':
-			if (bracket_stack.pop() != '[') {
-				throw "Syntax error";
-			}
-			// TODO pop the node_stack into the children of the tail
-			break;
-			default:
-			label_re.lastIndex = i;
-			bisegment_re.lastIndex = i;
-			var label_match = serialized_tree.match(label_re);
-			var bisegment_match = serialized_tree.match(bisegment_re);
-			if (label_match) {
-				var label = label_match[0];
-				var inverted = bracket_stack[bracket_stack.length-1] === "<";
-				i = label_re.lastIndex - 1;
-				var node = new Node(label, inverted);
-				node_stack.push(node);
-			} else {
+	while (true) {
+		var match = node_re.exec(serialized_tree);
+		serialized_tree = serialized_tree.substr(node_re.lastIndex);
+		node_re.lastIndex = 0;
 
-				if (label_match) {
-					var label = label_match[0];
-					var inverted = bracket_stack[bracket_stack.length-1] === "<";
-					i = bisegment_re.lastIndex - 1;
-					var node = new Node(label, inverted);
-					node_stack.push(node);
+		if (!match) {
+			console.log(current_node);
+			throw "Could not recognize: " + serialized_tree;
+		}
+
+		var leading_bracket = match[1];
+		var ending_bracket = match[5];
+
+		if (current_node) {console.log(current_node.label());}
+		
+		if (ending_bracket) {
+			// There was an ending bracket!
+			var reached_end_of_input = node_re.lastIndex == serialized_tree.length;
+			if (ending_bracket === ">" && current_node.inverted() ||
+				ending_bracket === "]" && !current_node.inverted()) {
+				// Completed node
+				if (!current_node.parent()) {
+					if (reached_end_of_input) {
+						return current_node;
+					} else {
+						throw "Completed tree before reaching end of input: " + serialized_tree.substr(node_end_re.lastIndex);
+					}
+				} else {
+					current_node = current_node.parent();
 				}
+			} else {
+				console.log(current_node);
+				throw "Missmatching bracket: " + ending_bracket;
 			}
+		} else if (leading_bracket) {
+			// There was a leading bracket! Good.
+			var inverted = match[1] === '<';
+			var label = match[2];
+			var bisegment;
+			if (match[3]) {
+				bisegment = {l1:match[3], l0:match[4]};
+			}
+			var new_node = new node_t(label, inverted, bisegment);
+			if (current_node) {
+				// Associate parent and child
+				current_node.add_child(new_node);
+				new_node.parent(current_node);
+			}
+			current_node = new_node;
 		}
 	}
 }
